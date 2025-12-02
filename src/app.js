172320567a950c8f2319a3ead611a3d38146016d
iamanu26 <anuragdubey26202@gmail.com>
2025-12-02 19:18:06 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express, { urlencoded } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb" , limit:"16kb"})) // here you are giving the limit of json file
+app.use(express.urlencoded({extended:true}))// if you dont give any thing it will also work
+                                            // it is yoused for url syntax
+app.use(express.static("public"))
+
+app.use(cookieParser()) // it use to store some thing to the user cookie and only server can read it
+
+export { app }
---
+++ src/utils/asyncHandlers.js
@@ -0,0 +1,30 @@
+const asyncHandler = (requestHandler)=>{
+    (req , res , next)=> {
+        Promise.resolve(requestHandler(req , res , next)).catch((err) => next(err))
+        
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// another way by using higher order function
+//=============> example
+// const asyncHandler = () ={}
+// const asyncHandler = (function)=>() ={}
+// const asyncHandler = (function)=> async() ={}
+
+//=========main code===============
+
+// const asyncHandler = (fn) => async(req , res , next) =>{
+//     try {
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
