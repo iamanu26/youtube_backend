4dc1bdc916d2b8d18c7911545558df0b2ca53eb3
iamanu26 <anuragdubey26202@gmail.com>
2025-12-07 01:13:59 +0530

    models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose , {Schema} from "mongoose";
-
+import jwt from  "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -19,7 +20,7 @@ const userSchema = new Schema(
             trim: true,
             
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -53,5 +54,43 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+//Here we don't use the arrow functio()=>{} because it dont have a current context access that is of (this)
+userSchema.pre("save" , async function(next) {
+    //when ever user access the user model it run the all field , so we dont have to save the password again and again we are using this if condition check
 
+    if(!this.Modified("password")) return next()
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+} )
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await  bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            email:this.email,
+            username: this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+           
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User" , userSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
-import mongoose, {Schema} from "mongoose";
-
+import mongoose, {Schema, Types} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
     {
         videoFile: {
@@ -22,10 +22,18 @@ const videoSchema = new Schema(
             type: Number,
             default: 0
         },
+        isPublished: {
+            type : Boolean
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
     },
     {
         timestamps: true
     }
 )
 
+videoSchema.plugin(mongooseAggregatePaginate)
 export const Video = mongoose.model("Video" , videoSchema)
