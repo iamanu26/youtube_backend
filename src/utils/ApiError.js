54897f8f52233da9c9adfb1d569ea527c0470f66
iamanu26 <anuragdubey26202@gmail.com>
2026-01-17 03:04:47 +0530

    fix: update ApiError class to correctly handle error messages and status

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -5,14 +5,15 @@ class ApiError extends Error{
         
         statusCode,
         message = "Something went wrong",
-        error = [],
+        errors = [],
         stack=""
     ){
         super(message)
         this.statusCode = statusCode
         this.data = null
         this.message = message
-        this.success = error
+        this.success = false;
+        this.errors = errors
 
         if(stack){
             this.stack = stack
